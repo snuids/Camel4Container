ff822162c4dd048ebf8c369c7e077fcb3935801c
Arnaud Marchand <amarchand@icloud.com>
2025-02-23 17:17:31 +0100

    v0.7

--- src/main/java/com/config/CamelContextLoader.java
+++ src/main/java/com/config/CamelContextLoader.java
@@ -7,25 +7,41 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanInfo;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CamelContextLoader {
+
     public static void main(String[] args) {
-        if (args.length <1) {
+        if (args.length < 1) {
             System.out.println("Usage: java -jar <jarfile> path/to/camel-context.xml");
             System.exit(1);
         }
-        String path = "file:"+args[0];
+        String path = "file:" + args[0];
         Path filePath = Paths.get(args[0]);
-        
+
         // Load the Spring context from the external XML file
-        ClassPathXmlApplicationContext applicationContext = 
-                new ClassPathXmlApplicationContext(path);
+        ClassPathXmlApplicationContext applicationContext
+                = new ClassPathXmlApplicationContext(path);
 
         // Get the Camel context from the Spring context
         CamelContext camelContext = applicationContext.getBean(CamelContext.class);
+        org.apache.camel.component.activemq.ActiveMQComponent amq = applicationContext.getBean(org.apache.camel.component.activemq.ActiveMQComponent.class);
 
         try {
             // Start the Camel context
@@ -34,20 +50,28 @@ public class CamelContextLoader {
             WatchService watchService = FileSystems.getDefault().newWatchService();
             filePath.getParent().register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
             System.out.println("Camel context started...");
+            MBeanServer mBeanServer = camelContext.getManagementStrategy().getManagementAgent().getMBeanServer();
+            while (true) {
 
-            while(true){
-                WatchKey key = watchService.take();
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY &&
-                            event.context().toString().equals(filePath.getFileName().toString())) {
-                        System.out.println("File " + filePath + " has changed.");
-                        System.exit(1);
-                        // Handle the file change (e.g., reload the context)
-                    }
+                if ((mBeanServer != null) && (amq != null)) {
+                    computeRouteStatus(mBeanServer, amq);
                 }
-                key.reset();
+
+                WatchKey key = watchService.poll(5, TimeUnit.SECONDS);
+                if (key != null) {
+                    for (WatchEvent<?> event : key.pollEvents()) {
+                        if (event.kind() == StandardWatchEventKinds.ENTRY_MODIFY
+                                && event.context().toString().equals(filePath.getFileName().toString())) {
+                            System.out.println("File " + filePath + " has changed.");
+                            System.exit(1);
+                            // Handle the file change (e.g., reload the context)
+                        }
+                    }
+                    key.reset();
+                }
+
                 System.out.println("Sleeping...");
-                Thread.sleep(5000);
+                //Thread.sleep(5000);
             }
             // Keep the application running for a while to process routes
             //Thread.sleep(5000);
@@ -60,5 +84,43 @@ public class CamelContextLoader {
             // Close the Spring context
             applicationContext.close();
         }
+    }
+
+    public static List<Map<String, Object>> computeRouteStatus(MBeanServer mBeanServer, org.apache.camel.component.activemq.ActiveMQComponent amq) {
+        List<Map<String, Object>> routesList = new ArrayList<>();
+
+        try {
+            Set<ObjectName> routes = mBeanServer.queryNames(new ObjectName("org.apache.camel:context=*,type=routes,*"), null);
+
+            for (ObjectName route : routes) {
+                Map<String, Object> routeAttributes = new HashMap<>();
+                routeAttributes.put("name", route.toString());
+
+                // Get MBean info to retrieve all attributes
+                MBeanInfo mBeanInfo = mBeanServer.getMBeanInfo(route);
+                MBeanAttributeInfo[] attributes = mBeanInfo.getAttributes();
+
+                for (MBeanAttributeInfo attributeInfo : attributes) {
+                    try {
+                        Object attributeValue = mBeanServer.getAttribute(route, attributeInfo.getName());
+                        routeAttributes.put(attributeInfo.getName(), attributeValue);
+                    } catch (Exception e) {
+                        routeAttributes.put(attributeInfo.getName(), "Failed to get attribute: " + e.getMessage());
+                    }
+                }
+
+                routesList.add(routeAttributes);
+            }
+            ObjectMapper objectMapper = new ObjectMapper();
+                    String jsonString = objectMapper.writeValueAsString(routesList);
+                    System.out.println("Routes JSON: " + jsonString);
+            ProducerTemplate producerTemplate = amq.getCamelContext().createProducerTemplate();
+            producerTemplate.sendBody("jms:topic:ROUTE_STATUS", jsonString);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return routesList;
     }
 }
